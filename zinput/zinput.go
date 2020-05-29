051e6227aa34923124f8ebcdf2abc0e2fe7e088d
zpbird <zpbird@qq.com>
2020-05-29 16:11:27 +0800

    m

--- zinput/zinput.go
+++ zinput/zinput.go
@@ -10,30 +10,37 @@ import (
 )
 
 const (
-	RegAll = ""
+	RegAll     = ""
+	RegAllRule = "任意字符："
+
 	// numbers
-	RegNum = ""
+	RegNum     = "^[0-9]*$"
+	RegNumRule = "[0-9]组成的纯数字："
 
 	// time
-	RegYear = "^[0-9]{1}[0-9]{3}$"
-	RegMon  = "^([1-9]{1}|0{1}[0-9]{1}|1{1}[0-2]{1})$"
-	RegDay  = ""
-	RegHms  = ""
+	RegYear       = "^[0-9]{1}[0-9]{3}$"
+	RegYearRul    = "[0000-9999]之间年份格式的数字："
+	RegMon        = "^([1-9]{1}|1{1}[0-2]{1})$"
+	RegMonRule    = "[1-12]之间月份格式的数字："
+	RegDay        = "^[1-9]{1}$|^[1-2]{1}[0-9]{1}$|^3[01]{1}$"
+	RegDayRule    = "[1-31]之间的数字："
+	RegHours      = "^[0-9]{1}$|^1[0-9]{1}$|^2[0-3]{1}$"
+	RegHoursRule  = "[0-23]之间的数字："
+	RegMinSec     = "^[0-9]{1}$|^[1-5]{1}[0-9]{1}$"
+	RegMinSecRule = "[0-59]之间的数字："
 
 	// phoneNumber
 	RegPhone = ""
 	// email
 	RegEmail = ""
+	// y or n (yes or no)
+	RegYn     = "^[yn]$"
+	RegYnRule = "[y或n]字符："
 )
 
 // --------------------------------------------------------------------------------
 // 工具函数------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
-
-// GetNum ...
-func GetNum() {
-
-}
 
 // --------------------------------------------------------------------------------
 // 基本函数------------------------------------------------------------------------
@@ -47,27 +54,36 @@ func Clear() {
 	cmd.Run()
 }
 
+// StopContinue ...
+func StopContinue() {
+	fmt.Printf("\n输入数据格式或数据范围不正确!\n回车键重新选择")
+	var t string //Scanln的临时变量
+	fmt.Scanln(&t)
+
+}
+
 // Input 基本输入
-func Input(termName string, regexpText string) (inputText string) {
-	Clear()
+func Input(termString string, regexpText string) (inputText string) {
 	var trigger = false
 	for !trigger {
-		fmt.Printf("请输入 %s : ", termName)
+		fmt.Printf("请输入%s", termString)
 		reader := bufio.NewReader(os.Stdin)
 		inputBytes, _, err := reader.ReadLine()
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Occur error when input", termName, ":", err)
-			continue
+			fmt.Fprintln(os.Stderr, "Occur error when input", termString, ":", err)
+			continue //??? os.Exit(0)
 		}
+
 		inputText = string(inputBytes)
 		trigger, err = regexp.MatchString(regexpText, inputText)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Occur error when match", termName, "(", inputText, "):", err)
-			continue
+			fmt.Fprintln(os.Stderr, "Occur error when match", termString, "(", inputText, "):", err)
+			continue //??? os.Exit(0)
 		}
+
 		if !trigger {
+			StopContinue()
 			Clear()
-			fmt.Fprintln(os.Stdout, termName, "(", inputText, ") -> 输入数据格式或数据范围不正确!")
 		}
 	}
 	return
